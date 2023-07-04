872f72588541326040e0e4a1ce1692b2f95db793
Sebastian Dümcke <code@sam-d.com>
2023-07-04 12:40:14 +0200

    rename ec2.scm to ecs.scm, fix imports and procedures
    * add exports
    * add missing imports
    * fix remove-entities
    * fix add-entities
    * add set-component and update-component
    * allow query to be either a procedure or an integer (sometimes list
    of integers FIXME)

--- ecs.scm
+++ ecs.scm
@@ -4,8 +4,19 @@
 	  world?
 	  <component>
 	  component?
-	  component)
-  (import (game bitset)
+	  component
+	  create-world
+	  add-entities
+	  remove-entities
+	  query
+	  get-components
+	  update-component
+	  set-component)
+  (import (rnrs base)
+	  (rnrs control)
+	  (rnrs hashtables)
+	  (rnrs records syntactic)
+	  (game bitset)
 	  (only (srfi :43) vector-copy))
 
   (define-record-type (<world> make-world world?)
@@ -45,12 +56,12 @@
 	(if (null? entities)
 	    (make-world ht v (apply set free) (set-union (all world) (apply set ids)))
 	    (let ((e (make-eq-hashtable (length (car entities)))))
-	  (for-each (lambda (comp)
-		      (hashtable-set! e (component-key comp) (component-value comp));fill entity hashtable with all components
-		      (hashtable-set! ht (component-key comp) (set-union (set (if (null? free) i (component-key free))) (hashtable-ref ht (component-key comp) (set))))) ;and fill map components->entities
-		    (car entities))
-	  (vector-set! v (if (null? free) i (car free)) e)
-	  (lp (if (null? free) (+ i 1) i) (if (null? free) free (cdr free)) (cdr entities) (cons (if (null? free) i (car free)) ids)))))))
+	      (for-each (lambda (comp)
+			  (hashtable-set! e (component-key comp) (component-value comp));fill entity hashtable with all components
+			  (hashtable-set! ht (component-key comp) (set-union (set (if (null? free) i (car free))) (hashtable-ref ht (component-key comp) (set))))) ;and fill map components->entities
+			(car entities))
+	      (vector-set! v (if (null? free) i (car free)) e)
+	      (lp (if (null? free) (+ i 1) i) (if (null? free) free (cdr free)) (cdr entities) (cons (if (null? free) i (car free)) ids)))))))
 
   ;syntax query returns a function that when applied to a world returns the set of ids that match the query (as a bitset)
   (define (get world type) (hashtable-ref (component-map world) type (set)))
@@ -67,19 +78,19 @@
       ((_ world (not e1 e2 e3 ...)) (set-difference (query-helper world) (query-helper world (and e1 e2 e3 ...))))
       ((_ world type) (get world type))
       ((_ world e1 e2 e3 ...) (query-helper world (and e1 e2 e3 ...))))) ;default conjunction is and
-
+  ;query can be either a function as returned by the query syntactic form that will return a bitset when applied to a world or a bitset
   (define (remove-entities world query)
-    (let ((ids (query world))
+    (let ((ids (if (procedure? query) (query world) (set query)))
 	  (ht (hashtable-copy (component-map world) #t))
 	  (v (vector-copy (entity-vector world))))
       (for-each (lambda (i)
-		  (vector-for-each (lambda (comp) (hashtable-update! ht comp (lambda (x) (set-difference x (set i)) (set)) #f)) (hashtable-keys (vector-ref v i))) ;remove entity id from the component map for all components it implements
+		  (vector-for-each (lambda (comp) (hashtable-update! ht comp (lambda (x) (set-difference x ids)) (set))) (hashtable-keys (vector-ref v i))) ;remove entity id from the component map for all components it implements
 		  (vector-set! v i #f)) ;clear the position in the vector
 		(set->list ids))
       (make-world ht v (set-union (free-ids world) ids) (set-difference (all world) ids))))
   
   (define (add-components world query . list-of-components)
-    (let ((ids (query world))
+    (let ((ids (if (procedure? query) (query world) query))
 	  (ht (hashtable-copy (component-map world) #t))
 	  (v (vector-copy (entity-vector world))))
       (when (null? (set->list ids)) (assertion-violation 'add-components "can only add components to existing entities but query returns an empty set"))
@@ -93,8 +104,39 @@
 		(set->list ids))
       (make-world ht v (free-ids world) (all world))))
 
+  ;return all the components values as values, but the first value returned is always the id of the entity from which the component is from
   (define (get-components world query . components)
-    (let ((ids (query world)))
-       (apply values (map (lambda(comp) (map (lambda (id) (hashtable-ref (vector-ref (entity-vector world) id) comp #f)) (set->list ids))) components))))
+    (let ((ids (if (procedure? query) (set->list (query world)) query)))
+       (apply values (cons ids (map (lambda(comp) (map (lambda (id) (hashtable-ref (vector-ref (entity-vector world) id) comp #f)) ids)) components)))))
 
+  ;for a query or integer, update all components in list components to the values in list values
+  (define (set-component world query components values)
+    (unless (and (list? components) (list? values) (= (length components) (length values))) (assertion-violation 'set-component "components and values must be lists of equal lengths" components values))
+    (let ((ids (if (procedure? query) (set->list (query world)) (list query)))
+	  (v (vector-copy (entity-vector world))))
+      (let loop ((i ids)
+		 (comp components)
+		 (val values)
+		 (ht (hashtable-copy (vector-ref v (car ids)) #t)))
+	(cond ((null? i) #f)
+	      ((not ht) (loop i comp val (hashtable-copy (vector-ref v (car i)) #t)))
+	      ((null? comp) (loop (cdr i) components values #f))
+	      (else (hashtable-set! ht (car comp) (car val))
+		    (vector-set! v (car i) ht)
+		    (loop i (cdr comp) (cdr val) ht))))
+      (make-world (component-map world) v (free-ids world) (all world))))
+
+  ;update the value of component 'component' for all entities matching query 'query' by calling the procedure with the previous value or default if the entity has no component 'component'
+  ;query is either a procedure create by the query syntactic form or an integer
+  (define (update-component world query component procedure default)
+    (unless (symbol? component) (assertion-violation 'update-component "component needs to be a single symbol" component))
+    (let ((ids (if (procedure? query) (set->list (query world)) (list query)))
+	  (v (vector-copy (entity-vector world))))
+      (for-each (lambda(id)
+		  (let ((ht (hashtable-copy (vector-ref v id) #t)))
+		    (hashtable-update! ht component procedure default)
+		    (vector-set! v id ht))) ids)
+      (make-world (component-map world) v (free-ids world) (all world))))
   )
+
+
